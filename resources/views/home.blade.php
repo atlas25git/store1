ded2d2ffd3b93aa8a303c81749ddfcca379829f4
atlas25git <atlas.sarthak@gmail.com>
2021-06-22 20:25:06 +0530

    3Start

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="container text-center">
+  
+  <h2>Products</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+   <div class="row">
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+   @foreach($allProducts as $products)
+
+   <div class="card col-3">
+       <img class="card-img-top" src="{{asset('default.jpg')}}" alt="Card image cap">
+       <div class="card-body">
+           <h4 class="card-title">{{$products->name}}</h4>
+           <p class="card-text">Text</p>
+       </div>
+       <div class="card-body">
+           <a href="#" class="card-link">Another link</a>
+       </div>
+   </div>
+
+    @endforeach
+
+   </div>
 </div>
 @endsection
